1010ecf930ac7cae66fc1ff754df45470ae5f5dd
anitha.b.m <anitha.b.m@CDC3-L-9V9FW93.dir.svc.accenture.com>
2025-03-06 14:33:16 +0530

    Second commit

--- GitHubProject/src/GitHubProject/Dropdown.java
+++ GitHubProject/src/GitHubProject/Dropdown.java
@@ -1,16 +1,17 @@
 package GitHubProject;
 
 
-public class Dropdown {
+public class Dropdown 
+{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			String hungry = "Yes";
 			
 			if(hungry == "Yes")
-				System.out.println("Yes, Im hungry");
+				System.out.println("Yes, Im Hungry");
 			else
-				System.out.println("No I'm not hungry");
+				System.out.println("No I'm not Hungry");
 	
 
 	}
