bdcae31921574972885894665c62731275fa9d86
anitha.b.m <anitha.b.m@CDC3-L-9V9FW93.dir.svc.accenture.com>
2025-03-06 13:42:06 +0530

    First commit!!!

---
+++ GitHubProject/src/GitHubProject/Dropdown.java
@@ -0,0 +1,18 @@
+package GitHubProject;
+
+
+public class Dropdown {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			String hungry = "Yes";
+			
+			if(hungry == "Yes")
+				System.out.println("Yes, Im hungry");
+			else
+				System.out.println("No I'm not hungry");
+	
+
+	}
+
+}
